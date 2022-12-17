50f3cf45bbbb7f97c1b5a7c453e3be6ece7072ff
mdebusk <mdebusk@gmail.com>
2022-12-17 15:29:43 -0500

    Removed helper function and added some new remaps

--- .config/nvim/lua/remaps.lua
+++ .config/nvim/lua/remaps.lua
@@ -2,7 +2,7 @@
 -- Filename   : remaps.lua                                                   --
 -- Author     : Michael DeBusk (https://gitlab.com/mdebusk/)                 --
 -- Created    : 2022-12-15 18:00                                             --
--- Last edit  : 2022-12-15 19:11                                             --
+-- Last edit  : 2022-12-17 14:58                                             --
 -- Purpose    : Keymappings for neovim                                       --
 -- Reference  : x                                                            --
 -- Depends    : neovim, keymap.lua                                           --
@@ -10,30 +10,32 @@
 -- TODO       : N/A                                                          --
 -------------------------------------------------------------------------------
 
-local keymap = require("keymap");
-
-local nnoremap = keymap.nnoremap;
-
 -- Disable arrow keys
-nnoremap("<Up>", "<NOP>");
-nnoremap("<Down>", "<NOP>");
-nnoremap("<Left>", "<NOP>");
-nnoremap("<Right>", "<NOP>");
+vim.keymap.set("n", "<Up>", "<NOP>");
+vim.keymap.set("n", "<Down>", "<NOP>");
+vim.keymap.set("n", "<Left>", "<NOP>");
+vim.keymap.set("n", "<Right>", "<NOP>");
 
 -- Keep the cursor centered during half-page jumps, search, and join
-nnoremap("n", "nzzzv");
-nnoremap("<C-d>", "<C-d>zz");
-nnoremap("N", "Nzzzv");
-nnoremap("<C-u>", "<C-u>zz");
-nnoremap("J", "mzJ`z");
+vim.keymap.set("n", "n", "nzzzv");
+vim.keymap.set("n", "<C-d>", "<C-d>zz");
+vim.keymap.set("n", "N", "Nzzzv");
+vim.keymap.set("n", "<C-u>", "<C-u>zz");
+vim.keymap.set("n", "J", "mzJ`z");
 
 -- Undo break points
--- inoremap(",", ",<c-g>u");
--- inoremap(".", ".<c-g>u");
--- inoremap("!", "!<c-g>u");
--- inoremap("?", "?<c-g>u");
+vim.keymap.set("i", ",", ",<c-g>u");
+vim.keymap.set("i", ".", ".<c-g>u");
+vim.keymap.set("i", "!", "!<c-g>u");
+vim.keymap.set("i", "?", "?<c-g>u");
 
 -- Don't leave visual mode when (un)indenting
--- vnoremap("<", "<gv");
--- vnoremap(">", ">gv");
+vim.keymap.set("v", "<", "<gv");
+vim.keymap.set("v", ">", ">gv");
+
+-- In case you open a file without sudo permissions
+vim.keymap.set("c", "w!!", "execute 'silent! write !sudo tee % >/dev/null' <bar> edit!");
+
+-- Open help in a vertical split and to the right
+vim.keymap.set("c", "help", "vertical botright help");
 
