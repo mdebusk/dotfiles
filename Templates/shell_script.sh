183bb6e4b854b0c6ac1a251719739accffb51011
mdebusk <mdebusk@gmail.com>
2024-03-14 13:11:32 -0400

    Replaced license with 'unlicense'; some minor reformatting

--- Templates/shell_script.sh
+++ Templates/shell_script.sh
@@ -11,39 +11,37 @@
 # TODO       : N.A                                                            #
 ###############################################################################
 
-# License and copyright {{{
+# ** License {{{
 ###############################################################################
-# Copyright © 2022 Michael DeBusk (https://gitlab.com/mdebusk/)               #
-# Template copyright © 2007, 2019 David Both LinuxGeek46@both.org             #
-# https://opensource.com/article/19/12/bash-script-template                   #
-# Full GNU General Public License can be viewed here:                         #
-# https://www.gnu.org/licenses/gpl-3.0.en.html                                #
+# This is free and unencumbered software released into the public domain.     #
 #                                                                             #
-# This program is free software; you can redistribute it and/or               #
-# modify it under the terms of the GNU General Public License as              #
-# published by the Free Software Foundation; either version 2 of              #
-# the License, or (at your option) any later version.                         #
+# Anyone is free to copy, modify, publish, use, compile, sell, or             #
+# distribute this software, either in source code form or as a compiled       #
+# binary, for any purpose, commercial or non-commercial, and by any means.    #
 #                                                                             #
-# This program is distributed in the hope that it will be useful,             #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
-# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. See the GNU            #
-# General Public License for more details.                                    #
+# In jurisdictions that recognize copyright laws, the author or authors of    #
+# this software dedicate any and all copyright interest in the software to    #
+# the public domain. We make this dedication for the benefit of the public    #
+# at large and to the detriment of our heirs and successors. We intend        #
+# this dedication to be an overt act of relinquishment in perpetuity of       #
+# all present and future rights to this software under copyright law.         #
 #                                                                             #
-# You should have received a copy of the GNU General Public                   #
-# License along with this program; if not, write to:                          #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,             #
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF          #
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.      #
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER     #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING     #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER         #
+# DEALINGS IN THE SOFTWARE.                                                   #
 #                                                                             #
-# The Free Software Foundation, Inc.                                          #
-# 59 Temple Place, Suite 330                                                  #
-# Boston, MA 02111-1307                                                       #
+# For more information, please refer to <http://unlicense.org/>               #
 ###############################################################################
 #  End license and copyright }}}
 
-# Help {{{
 set -euo pipefail # catch a variety of errors
 
-Help()
-{
-    cat <<EOF
+Help() { # {{{
+    cat << EOF
 Add description of the script functions here.
 
 Syntax: $0 [-g|h|v|V]
@@ -54,67 +52,78 @@ v     Verbose mode.
 V     Print software version and exit.
 
 EOF
-}
-# End help }}}
+} # }}}
 
-# License display {{{
-License()
-{
+
+License() { # {{{
     cat <<EOF
-$0 Copyright © 2022 Michael DeBusk
+This is free and unencumbered software released into the public domain.
 
-This program comes with ABSOLUTELY NO WARRANTY. This is free
-software, and you are welcome to redistribute it under certain
-conditions. See https://www.gnu.org/licenses/gpl-3.0.en.html
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any means.
 
+In jurisdictions that recognize copyright laws, the author or authors of
+this software dedicate any and all copyright interest in the software to
+the public domain. We make this dedication for the benefit of the public
+at large and to the detriment of our heirs and successors. We intend
+this dedication to be an overt act of relinquishment in perpetuity of
+all present and future rights to this software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
 EOF
-}
-# End license display }}}
+} # }}}
 
-# check for root {{{
-checkRoot()
-{
-   # If we are not running as root we exit the program
-   if [ `id -u` != 0 ]
-   then
-      echo "ERROR: You must be root user to run this program"
-      exit
-   fi
-}
-# End check for root }}}
 
-# {{{Main program
+checkRoot() { # {{{
+    # If we are not running as root we exit the program
+    if [ `id -u` != 0 ]
+    then
+        echo "ERROR: You must be root user to run this program"
+        exit
+    fi
+} # }}}
 
-# Sanity checks {{{2
+# ** Main program {{{
+
+# ** Sanity checks {{{2
 #
 # Are we root? If not, then quit
 # checkRoot
 # End sanity checks }}}
 
-# Initialize variables {{{2
+# ** Initialize variables {{{2
 option=""
 Version="0.1"
 Msg="Hello world!"
 # End variables }}}
 
-# Process the input options. Add options as needed. {{{2
+# ** Process the input options. Add options as needed. {{{2
 #
 # Get the options
 while getopts ":ghV" option; do
-   case $option in
-      h) # display Help
-         Help
-         exit;;
-      g) # display license
-         License
-         exit;;
-      V) # display version
-         echo -e "$0 version $Version\n"
-         exit;;
-     \?) # incorrect option
-         echo "Error: Invalid option"
-         exit;;
-   esac
+    case $option in
+        h) # display Help
+            Help
+            exit;;
+        g) # display license
+            License
+            exit;;
+        V) # display version
+            echo -e "$0 version $Version\n"
+            exit;;
+        \?) # incorrect option
+            echo "Error: Invalid option"
+            exit;;
+    esac
 done
 # End options }}}
 
